8d454a2bddd17b3b84a0fee1a32cb39d646f1eff
rogerio <rogeriolana9@gmail.com>
2025-05-18 20:31:12 -0300

    primeiro commit

---
+++ cypress/e2e/ct001_pacto.cy.js
@@ -0,0 +1,27 @@
+describe('Buscar Pacto Soluções no yahoo ', () => {
+  it('Deve realizar busca no Yahoo por Pacto Soluções', () => {
+
+    //Acessar site do Yahoo
+    cy.visit('https://br.search.yahoo.com/?fr2=p:fprd,mkt:br')
+
+    //Localizar caixa de busca e digitar "Pacto soluções"
+    cy.get('#yschsp').type('Pacto soluções')
+    cy.screenshot('antes-da-busca')
+
+    //Submeter a busca
+    cy.get('#yschsp').type('{enter}')
+
+    //Validar que a pagina de resultados foi carregada
+    cy.url().should('include', 'p=Pacto+solu%C3%A7%C3%B5es&fr')
+    cy.screenshot('pagina-de-resultados')
+
+    //Validar que os resultados contem a palavra "Pacto Soluções" ,"Sistema para Academias e Negócios Fitness:"
+
+    cy.get('#web > .reg > .first > .dd > .compTitle > .title > .fz-20')
+      .should('contain', 'Sistema para Academias e Negócios Fitness:')
+    cy.get(':nth-child(2) > .dd > .compText > .fz-14 > .fc-falcon > b')
+      .should('contain', 'Pacto Soluções')
+
+
+  })
+})
